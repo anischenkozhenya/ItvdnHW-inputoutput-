2125339922ad4893dc4b0d8ca845f79c889f0bbc
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-21 22:52:57 +0300

    add 1 task

--- Inputoutput/task1/Program.cs
+++ Inputoutput/task1/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+
+//Создайте на диске 100 директорий с именами от Folder_0 до Folder_99, затем удалите их.
 
 namespace task1
 {
@@ -10,6 +11,61 @@ namespace task1
     {
         static void Main(string[] args)
         {
+            //Путь к папке в которой будут создаваться и удаляться новые папки
+            string folder = @"..\..\WorkFolder\";
+
+
+            DirectoryInfo directory = new DirectoryInfo(folder);
+
+            //Открытие рабочей папки
+            Process.Start("explorer.exe", folder);
+
+            Console.WriteLine("Подпапок в папке " + directory.Name + " " + directory.GetDirectories().Length);
+            Console.WriteLine("Для удаления нажмите кнопку");
+            Console.ReadKey();
+
+            //Создание 100 новых папок
+            for (int i = 0; i < 100; i++)
+            {
+                directory.CreateSubdirectory("Folder_" + i.ToString());
+            }
+
+
+            Console.WriteLine("Подпапок в папке " + directory.Name + " " + directory.GetDirectories().Length);
+            Console.WriteLine("Для удаления нажмите кнопку");
+            Console.ReadKey();
+
+            //удаление папок
+
+            //Первый вариант
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    Directory.Delete(directory.ToString() + "\\Folder_" + i.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            //Второй вариант
+            //DirectoryInfo[] dirarray = directory.GetDirectories();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    foreach(var dir in dirarray)
+            //    {
+            //        if(dir.Name=="Folder_" + i.ToString())
+            //        {
+            //            dir.Delete();
+            //        }
+            //    }
+            //}
+            Console.WriteLine("Подпапок в папке " + directory.Name + " " + directory.GetDirectories().Length);
+            Console.WriteLine("Нажмите любую кнопку...");
+            Console.ReadKey();
+
         }
     }
 }
