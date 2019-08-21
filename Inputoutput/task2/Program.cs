e2373ac44062adc1d3f22fc3d61c2f1eadd88203
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-21 23:08:05 +0300

    add task2

---
+++ Inputoutput/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+//Создайте файл, запишите в него произвольные данные и закройте файл.Затем снова откройте
+//этот файл, прочитайте из него данные и выведете их на консоль.
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new FileStream("file.txt", FileMode.OpenOrCreate);
+            var strwrite = new StreamWriter(file);
+            Console.WriteLine("Введите строку текста в файл");
+            strwrite.WriteLine(Console.ReadLine());
+            strwrite.Close();
+            file.Close();
+            Console.WriteLine("Файл сохранен");
+            var strread = File.OpenText("file.txt");
+            Console.WriteLine("текст в файле:");
+            Console.WriteLine(strread.ReadLine());
+            strread.Close();
+            File.Delete("file.txt");
+            Console.ReadKey();
+        }
+    }
+}
