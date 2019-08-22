68796b552e563c1c3eb37f1bfdc58a26702c2d40
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-22 19:13:28 +0300

    add task3

---
+++ Inputoutput/task3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+using System.Windows.Forms;
+
+namespace task3
+{
+    public partial class Form1 : Form
+    {
+        //Массив путей
+        public string[] path = { };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Получение физических дисков
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (var drive in drives)
+            {
+                if (drive.IsReady)
+                {
+                    comboBox1.Items.Add(drive.Name.ToString());
+                }
+            }
+            comboBox1.Text = comboBox1.Items[0].ToString();
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            //Очистка Listbox
+            listBox1.Items.Clear();
+
+            //Обнуление переменной
+            path = new string[] {};
+
+            //Имя файла
+            string fileName = textBox1.Text;
+            DirectoryInfo directory = new DirectoryInfo(comboBox1.Text);
+            listBox1.Items.Add("search start");
+
+            //Поиск файлов в папках на диске
+            //нет доступа с системным папкам
+            Search(directory, fileName);
+            //Для примера с папками имя файла "exemple.txt"
+            //Search(new DirectoryInfo(@"..\..\.."), fileName);
+            listBox1.Items.Add("search finish");
+            void Search(DirectoryInfo dr, string file)
+            {
+                FileInfo[] files = dr.GetFiles();
+                foreach (var item in files)
+                {
+                    if (file == item.Name)
+                    {
+                        
+                        listBox1.Items.Add("Файл найден");
+                        listBox1.Items.Add(item.FullName);
+                        path = path.Add(item.FullName);
+                        StreamReader reader = File.OpenText(item.FullName.ToString());
+                        while (!reader.EndOfStream)
+                        {
+                            listBox1.Items.Add(reader.ReadLine());
+                        }
+                        reader.Close();
+                        listBox1.Items.Add("Весь файл записан");
+                    }
+                }
+                DirectoryInfo[] dirs = dr.GetDirectories();
+                foreach (DirectoryInfo directoryInfo in dirs)
+                {
+                    Search(directoryInfo, file);
+                }
+            }
+        }
+
+        private void BtnArchivin_Click(object sender, EventArgs e)
+        {
+            btnArchivin.Text= "Архивация...";
+            if (path.Length == 0)
+            {
+                MessageBox.Show("файл не найден");
+            }
+            else
+            {
+                for (int i = 0; i < path.Length; i++)
+                {
+                    FileStream fileStream = File.OpenRead(path[i]);
+                    FileStream fileStream2 = File.Create(path[i] + ".zip");
+                    GZipStream gZipStream = new GZipStream(fileStream2, CompressionMode.Compress);
+                    int bt = fileStream.ReadByte();
+                    while (bt != -1)
+                    {
+                        gZipStream.WriteByte((byte)bt);
+                        bt = fileStream.ReadByte();
+                    }
+                    gZipStream.Close();
+                    listBox1.Items.Add("Заархивировано");
+                }
+            }
+            btnArchivin.Text = "Заархивировано";
+        }     
+    }
+}
---
+++ Inputoutput/task3/myClass.cs
@@ -0,0 +1,19 @@
+﻿
+namespace task3
+{
+    public static class myClass
+    {
+        //Расширяющий метод
+        public static string[] Add(this string[] str, string path)
+        {
+            string[] a = str;
+            str = new string[str.Length + 1];
+            for (int i = 0; i < a.Length; i++)
+            {
+                str[i] = a[i];
+            }
+            str[str.Length - 1] = path;
+            return str;
+        }
+    }
+}
